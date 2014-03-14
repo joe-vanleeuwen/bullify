a6853ea0354671f15268782f8f1058a64ea50cb5
Joe VanLeeuwen <jvnlwn@gmail.com>
2014-03-14 12:47:13 -0400

    hello bullify

---
+++ list.js
@@ -0,0 +1,19 @@
+list = [
+    'First Main Point.',
+    [
+        'First main - first subpoint',
+        'First main - second subpoint',
+        'First main - third subpoint'
+    ],
+    'Second Main Point.',
+    [
+        'Second main - first subpoint',
+        'Second main - second subpoint',
+        [
+            'Second main - second subpoint - first point',
+            'Second main - second subpoint - second point',
+            'Second main - second subpoint - third point'
+        ],
+        'Second main - third subpoint'
+    ]
+]
